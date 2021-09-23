2a5f7d64b16cd416e748fc9c7cb7dba9eb714e88
callahamZ <91182185+callahamZ@users.noreply.github.com>
2021-09-23 22:13:29 +0700

    memisah balasan ke file berbeda
    perubahan #1

---
+++ balasan.py
@@ -0,0 +1,23 @@
+def sample_responses(input_text, namauser):
+    pesan_user = str(input_text).lower()
+    
+    #======================================
+    #Kategori balasan
+    sapaan = ["halo", "hi", "hai", "selamat pagi"]
+    covidinfo = ["covid", "info covid", "covid 19", "covid-19", "gejala covid"]
+    pamitan = ["sampai jumpa", "bye"]
+    #=========================================
+
+    #======================================
+    #Eksekusi
+    if pesan_user in sapaan:
+        return f"Halo {namauser}!!"
+
+    if pesan_user in covidinfo:
+        return f"{namauser} mau info covid? \n\nTunggu tim riset yaa... :3"
+
+    if pesan_user in pamitan:
+        return f"Sampai jumpa {namauser}"
+
+    return "Aku tidak mengerti"
+    #==========================================
