7c1b889b516c5f3e2cbb2c674a5e7b63a9dc6462
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-09-24 21:39:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,30 +54,30 @@ No. Telepon = {}
          pass
 
     # menu angka penyebaran covid setiap provinsi
-@bot.message_handler(commands=['covid'])
-def covid(message):
-   texts = message.text
-   provinsi = texts[7:]
-   page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-   page_json = page.json()
-   features = page_json['features']
-   for i in features:
-      prov = i['attributes']['Provinsi']
-      pos =  i['attributes']['Kasus_Posi']
-      sem = i['attributes']['Kasus_Semb']
-      men = i['attributes']['Kasus_Meni']
-      dirawat = int(pos)-int(sem)-int(men)
-      data = ('''
-Provinsi = {}
-Positif = {}
-Sembuh = {}
-Meninggal= {}
-Dirawat = {}
-'''.format(prov, pos, sem, men, dirawat))
-      if provinsi.upper() in prov.upper():
-         bot.reply_to(message, data)
-      else:
-         pass
+#@bot.message_handler(commands=['covid'])
+#def covid(message):
+ #  texts = message.text
+  # provinsi = texts[7:]
+   #page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+   #page_json = page.json()
+   #features = page_json['features']
+   #for i in features:
+    #  prov = i['attributes']['Provinsi']
+     # pos =  i['attributes']['Kasus_Posi']
+     # sem = i['attributes']['Kasus_Semb']
+     # men = i['attributes']['Kasus_Meni']
+     # dirawat = int(pos)-int(sem)-int(men)
+      #data = ('''
+#Provinsi = {}
+#Positif = {}
+#Sembuh = {}
+#Meninggal= {}
+#Dirawat = {}
+#'''.format(prov, pos, sem, men, dirawat))
+ #     if provinsi.upper() in prov.upper():
+  #       bot.reply_to(message, data)
+   #   else:
+    #     pass
 
 
 bot.polling()
