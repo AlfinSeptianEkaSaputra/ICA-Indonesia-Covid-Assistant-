f883857df1555dcf2cdda336ad8c7b287c2172b6
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-09-30 18:34:29 +0700

    Update balasan.py

--- balasan.py
+++ balasan.py
@@ -1,4 +1,4 @@
-import api as bot
+
 def sample_responses(input_text, namauser):
     pesan_user = str(input_text).lower()
     
@@ -14,35 +14,35 @@ def sample_responses(input_text, namauser):
     if pesan_user in sapaan:
         return f"Halo {namauser}!!"
 
-    #if pesan_user in covidinfo:
-        #return f"{namauser} mau info covid? \n\nTunggu tim riset yaa... :3"
+    if pesan_user in covidinfo:
+        return f"{namauser} mau info covid? \n\nTunggu tim riset yaa... :3"
 
     if pesan_user in pamitan:
         return f"Sampai jumpa {namauser}"
     
-    covid_info(message):
-        texts = message.text
-        provinsi = texts[7:]
-        page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-        page_json = page.json()
-        features = page_json['features']
-        for i in features:
-            prov = i['attributes']['Provinsi']
-            pos =  i['attributes']['Kasus_Posi']
-            sem = i['attributes']['Kasus_Semb']
-            men = i['attributes']['Kasus_Meni']
-            dirawat = int(pos)-int(sem)-int(men)
-            data = ('''
-        Provinsi = {}
-        Positif = {}
-        Sembuh = {}
-        Meninggal= {}
-        Dirawat = {}
-        '''.format(prov, pos, sem, men, dirawat))
-    if provinsi.upper() in prov.upper():
-           update.message.reply_text(message, data)
-    else:
-            pass
+    #covid_info(message):
+     #   texts = message.text
+      #  provinsi = texts[7:]
+       # page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+       # page_json = page.json()
+        #features = page_json['features']
+        #for i in features:
+         #   prov = i['attributes']['Provinsi']
+          #  pos =  i['attributes']['Kasus_Posi']
+           # sem = i['attributes']['Kasus_Semb']
+           # men = i['attributes']['Kasus_Meni']
+           # dirawat = int(pos)-int(sem)-int(men)
+           # data = ('''
+        #Provinsi = {}
+        #Positif = {}
+        #Sembuh = {}
+        #Meninggal= {}
+        #Dirawat = {}
+        #'''.format(prov, pos, sem, men, dirawat))
+    #if provinsi.upper() in prov.upper():
+     #      update.message.reply_text(message, data)
+    #else:
+     #       pass
 
     return "Aku tidak mengerti"
     #==========================================
