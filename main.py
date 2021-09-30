1b0f594d0c89fdc294734e286e6fe953c27dd285
callahamZ <91182185+callahamZ@users.noreply.github.com>
2021-09-30 08:43:02 +0700

    mengubah library main
    ganti menjadi library milik ilham 12

--- main.py
+++ main.py
@@ -1,58 +1,42 @@
-from telebot import *
-import requests
-from googlesearch import search
+import constant as keys
+from telegram.ext import *
+import responses as R
 
 
-api = '2021833578:AAFsk5f2SdbsY_1MXn00ORp8oaabY0YCJcY'
-bot = telebot.TeleBot(api)
+#print("Bot Berjalan")
+
+def start_command(update, context):
+    namauser = update.message.chat.first_name
+    update.message.reply_text(f'''Halo {namauser}\n\nSaya adalah >namabot< !\nSaya bisa membantu anda mencari infotmasi tentang Covid 19.\n\nAnda bisa mulai dengan mengirim\n/help untuk melihat segala fitur yang saya miliki''')
+def help_command(update, context):
+    update.message.reply_text('Butuh bantuan? maaf, bot belum dikembangkan sejauh itu')
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+    namauser = update.message.chat.first_name
+
+    print(f"{namauser} : Mengirimkan pesan > {text} <")
+
+    response = R.sample_responses(text, namauser)
+
+    update.message.reply_text(response)
 
 
-@bot.message_handler(commands=['start'])
-def selamat_datang(message):
-   bot.reply_to(message, 'Hai saya ICA (Indonesia Covid Assistant)')
-   chatid = message.chat.id
-   bot.send_message(chatid, 'Selamat Datang')
+def error(update, context):
+    print(f"update {update} menyebabkan error {context.error}")
 
-@bot.message_handler(content_types=['sticker'])
-def handle_sticker(message):
-    bot.send_sticker(message.chat.id, message.sticker.file_id)
+def main():
+    updater = Updater(keys.API_KEY, use_context=True)
+    dp = updater.dispatcher
 
-@bot.message_handler(commands=['google'])
-def google(message):
-   data = message.text.replace('/google', "")
-   x = search(data, num_results=2)
-   for i in x:
-      bot.send_message(message.chat.id, i)
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
 
-@bot.message_handler(commands=['covid'])
-def covid(message):
-   texts = message.text
-   provinsi = texts[7:]
-   page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
-   page_json = page.json()
-   features = page_json['features']
-   for i in features:
-      prov = i['attributes']['Provinsi']
-      pos =  i['attributes']['Kasus_Posi']
-      sem = i['attributes']['Kasus_Semb']
-      men = i['attributes']['Kasus_Meni']
-      dirawat = int(pos)-int(sem)-int(men)
-      data = ('''
-Provinsi = {}
-Positif = {}
-Sembuh = {}
-Meninggal= {}
-Dirawat = {}
-'''.format(prov, pos, sem, men, dirawat))
-      if provinsi.upper() in prov.upper():
-         bot.reply_to(message, data)
-      else:
-         pass
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
 
-# Set up the logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logging.info('Starting Bot...')
-bot.polling(True)
+    dp.add_error_handler(error)
 
+    updater.start_polling()
+    updater.idle()
 
-
+main()
