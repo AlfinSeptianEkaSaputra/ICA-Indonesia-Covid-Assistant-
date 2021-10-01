d196992f1667d0114eea53fba6c32163069530f0
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-10-01 09:43:23 +0700

    Update fitur.py

--- fitur.py
+++ fitur.py
@@ -1,2 +1,13 @@
 from telebot import *
+import api
 
+bot = telebot.TeleBot(api.API_KEYS)
+def sample_responses(input_text, namauser):
+    pesan_user = str(input_text).lower()
+    
+@bot.message_handler(commands=['google'])
+def google(message):
+   data = message.text.replace('/google', "")
+   x = search(data, num_results=2)
+   for i in x:
+      bot.send_message(message.chat.id, i)
