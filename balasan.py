e64cfd5557f889708cd4a86de50569bbc9219dd0
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-10-01 10:48:40 +0700

    Update balasan.py

--- balasan.py
+++ balasan.py
@@ -23,7 +23,7 @@ def sample_responses(input_text, namauser):
     if pesan_user in pamitan:
         return f"Sampai jumpa {namauser}"
    
-def covid(input_text, namauser):
+def covid_responses(input_text, namauser):
     pesan_user = str(input_text).lower()
   
     page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json')
