d0efc51801f3a55d98a6a15a751221ffae026878
callahamZ <91182185+callahamZ@users.noreply.github.com>
2021-10-02 18:34:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
+import api
 import api as keys
 from telegram.ext import *
 import balasan as R
+import fitur as F
 from googlesearch import search
 
 print("Bot Berjalan")
-def start_command(update, context):
-    namauser = update.message.chat.first_name
-    update.message.reply_text(f'''Halo {namauser}\n\nSaya adalah ICA(Indonesian Covid Asistant) !\nSaya bisa membantu anda mencari informasi tentang Covid 19.\n\nAnda bisa mulai dengan mengirim\n/help untuk melihat segala fitur yang saya miliki''')
-def help_command(update, context):
-    update.message.reply_text('Butuh bantuan? maaf, bot belum dikembangkan sejauh itu')
-
-def google(update, context):
-      text = str(update.message.text).lower()
-      hasilpencarian = search(text, num_results=2)
-      update.message.reply_text(hasilpencarian)
 
 def handle_message(update, context):
     text = str(update.message.text).lower()
@@ -32,9 +24,10 @@ def main():
     updater = Updater(keys.API_KEY, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(CommandHandler("google", google))
+    dp.add_handler(CommandHandler("start", F.start_command))
+    dp.add_handler(CommandHandler("help", F.help_command))
+    dp.add_handler(CommandHandler("google", F.google))
+    dp.add_handler(CommandHandler("covid", F.covid))
 
     dp.add_handler(MessageHandler(Filters.text, handle_message))
 
