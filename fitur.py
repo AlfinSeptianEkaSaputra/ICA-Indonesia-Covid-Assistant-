d44110875de29256539958594a874fff7007ecbb
callahamZ <91182185+callahamZ@users.noreply.github.com>
2021-10-03 09:01:47 +0700

    Menambahkan fitur help

--- fitur.py
+++ fitur.py
@@ -4,11 +4,26 @@ import requests
 def start_command(update, context):
    namauser = update.message.chat.first_name
    update.message.reply_text(
-      f'''Halo {namauser}\n\nSaya adalah ICA(Indonesian Covid Asistant) !\nSaya bisa membantu anda mencari informasi tentang Covid 19.\n\nAnda bisa mulai dengan mengirim\n/help untuk melihat segala fitur yang saya miliki''')
+      f'''Halo {namauser}\n\n'''
+      "Saya adalah ICA(Indonesian Covid Asistant). Saya bisa membantu anda mencari informasi tentang Covid 19.\n\n"
+      "Anda bisa mulai dengan mengirim\n/help untuk melihat segala fitur yang saya miliki")
 
 def help_command(update, context):
-   update.message.reply_text('Butuh bantuan? maaf, bot belum dikembangkan sejauh itu')
-
+   update.message.reply_text("Saya akan membantu anda mencari informasi seputar Covid-19\n\n"
+                             "Perintahkan saya  dengan klik atau masukkan command dibawah\n\n"
+                             "Info Covid\n"
+                             "/covid >provinsi< - Jumlah kasus covid di provinsi tersebut\n"
+                             ">>fitur<<\n"
+                             ">>fitur<<\n"
+                             ">>fitur<<\n"
+                             ">>fitur<<\n\n"
+                             "Anda bisa juga konsultasi gejala covid, saya akan membantu anda\n\n"
+                             "Kami juga.... >>tambahkan<<\n"
+                             ">>fitur<<\n"
+                             ">>fitur<<\n"
+                             ">>fitur<<\n\n"
+                             "Atau hanya anda ingin mengobrol? Tidak apa-apa. Saya akan menemani anda :)")
+   
 def google(update, context):
    text = str(update.message.text).lower()
    hasilpencarian = search(text, num_results=1)
