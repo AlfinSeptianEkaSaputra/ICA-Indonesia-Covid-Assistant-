a0b2710af28c940bc815fad10be2dc9f2dfd7fc2
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-10-13 12:37:47 +0700

    Update balasan.py

--- balasan.py
+++ balasan.py
@@ -7,7 +7,7 @@ def sample_responses(input_text, namauser):
     #======================================
     #Kategori balasan
     perkenalan = ["apa kabar", "siapa kamu", "siapa namamu", "kamu siapa"]
-    sapaan = ["halo", "hi", "hai", "selamat pagi"]
+    sapaan = ["halo", "hi", "hai", "selamat pagi", "selamat siang", "selamat sore", "selamat mlam"]
     covidinfo = ["covid", "info covid", "covid 19", "covid-19", "gejala covid"]
     pamitan = ["sampai jumpa", "bye", "selamat tinggal"]
     #=========================================
@@ -19,7 +19,7 @@ def sample_responses(input_text, namauser):
         return sapakembali
     
     if pesan_user in covidinfo:
-        return f"{namauser} mau info covid? Silahkan tekan /covid untuk info seputar update kasus Covid-19"
+        return f"{namauser} mau info covid? Silahkan tekan /help untuk melihat fitur saya."
                  
     if pesan_user in perkenalan:
         return f"Saya adalah ICA, Indonesian Covid Assistant\nSaya akan membantu anda mencari informasi seputar Covid-19"
