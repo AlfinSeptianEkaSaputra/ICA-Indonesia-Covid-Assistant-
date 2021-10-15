3220dc8a1a24f3c96e5bf386146192f70ae31e80
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-10-15 18:10:01 +0700

    Update fitur.py

--- fitur.py
+++ fitur.py
@@ -1,8 +1,8 @@
 from googlesearch import search
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 import requests
-
-#===============#DATA================      
+import datetime
+#===============#DATA================
 my_states = ['Aceh', 'Sumatera Utara', 'Sumatera Barat', 'Riau', 'Jambi', 'Sumatera Selatan', 'Bengkulu', 'Bangka Belitung', 'Lampung', 'Kepulauan Riau', 'DKI Jakarta', 'Jawa Barat', 'Jawa Tengah', 'Daerah Istimewa Yogyakarta', 'Jawa Timur', 'Banten', 'Bali',
              'Nusa Tenggara Barat', 'Nusa Tenggara Timur', 'Kalimantan Barat', 'Kalimantan Tengah', 'Kalimantan Selatan', 'Kalimantan Timur', 'Kalimantan Utara', 'Sulawesi Utara', 'Sulawesi Tengah', 'Sulawesi Selatan', 'Sulawesi Tenggara', 'Gorontalo', 'Sulawesi Barat', 'Maluku', 'Maluku Utara', 'Papua', 'Papua Barat']
 
@@ -13,7 +13,20 @@ regional_data = res['features']
 total_states = int(len(regional_data))
 #=====================================
 
+def realtime(update, perintah):
+    text = str(update.message.text).lower()
+    waktu = datetime.datetime.now()
+    tanggal = waktu.strftime('%D')
+    namauser1 = update.message.chat.first_name
+    namauser2 = update.message.chat.last_name
+    data = (f"{tanggal}:{namauser1} {namauser2} => Mengirimkan pesan >{text}<\n")
+    log_bot = open('data_pesan_user.txt', 'a')
+    log_bot.write(data)
+    log_bot.close()
+
 def start_command(update, context):
+    realtime(update, 'start')
+    
     namauser = update.message.chat.first_name
     update.message.reply_text(
         f'''Halo {namauser}\n\n'''
@@ -21,13 +34,15 @@ def start_command(update, context):
         "Anda bisa mulai dengan mengirim\n/help untuk melihat segala fitur yang saya miliki")
 
 def help_command(update, context):
+    realtime(update, 'help')
+    
     update.message.reply_text("Saya akan membantu anda mencari informasi seputar Covid-19\n\n"
                               "Perintahkan saya  dengan klik atau masukkan command dibawah\n\n"
                               "Info Covid\n"
-                              "/indonesia : Kasus covid-19 di Indonesia 🇮🇩\n"
-                              "/covidprov : Kasus COVID Per Provinsi di Indonesia 🦠\n"
-                              "/berita  : Berita Seputar Covid19 📺\n"
-                              "/rumahsakit  : Rumah Sakit Rujukan Nasional 🏥\n"
+                              "/indonesia => Kasus covid-19 di Indonesia 🇮🇩\n"
+                              "/covid => Kasus Per Provinsi COVID-19 di Indonesia 🦠\n"
+                              "/berita => Menampilkan Berita Seputar Covid19 📺\n"
+                              "/rumahsakit => Rumah Sakit Rujukan Nasional 🏥\n"
                               ">>fitur<<\n\n"
                               "Anda bisa juga konsultasi gejala covid, saya akan membantu anda\n\n"
                               "Kami juga.... >>tambahkan<<\n"
@@ -37,6 +52,8 @@ def help_command(update, context):
                               "Atau hanya anda ingin mengobrol? Tidak apa-apa. Saya akan menemani anda :)")
 
 def state_wise(update, context):
+    realtime(update, 'covidprov')
+    
     keyboard = [[InlineKeyboardButton("Aceh", callback_data='Aceh'),
                  InlineKeyboardButton("Sumatera Utara", callback_data='Sumatera Utara')],
 
@@ -111,9 +128,11 @@ Kasus Meninggal :  *{regional_data[index]['attributes']['Kasus_Meni']:,}*"
 
 
 def rumkit(update, context):
+   realtime(update, 'rumahsakit')
+    
    texts = update.message.text
    if (texts == "/rumahsakit"):
-       update.message.reply_text("Tolong tambahkan wilayah atau alamat rumah sakit di daerah yang anda cari 🏥.\n\n/rumahsakit *wilayah atau alamat rumah sakit*.\nMisalkan /rumahsakit Kediri.\nAtaupun /rumahsakit  JL. VETERAN...")
+       update.message.reply_text("Tolong tambahkan wilayah atau alamat rumah sakit di daerah yang anda cari.\n\n/rumahsakit *wilayah atau alamat rumah sakit*.\nMisalkan /rumahsakit Kediri.\nAtaupun /rumahsakit  JL. VETERAN...")
    else:
        pesan = texts[12:]
        page = requests.get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/RS_Rujukan_Update_May_2020/FeatureServer/0/query?where=1%3D1&outFields=nama,alamat,wilayah,kode_rs,telepon&outSR=4326&f=json')
@@ -141,6 +160,8 @@ def rumkit(update, context):
    update.message.reply_text("Ketik /help untuk kembali ke *MENU FITUR*.",parse_mode="MARKDOWN")
 
 def google(update, context):
+   realtime(update, 'berita')
+    
    data = update.message.text
 
    if (data == "/berita"):
@@ -153,6 +174,10 @@ def google(update, context):
    update.message.reply_text("Ketik /help untuk kembali ke *MENU FITUR*.",parse_mode="MARKDOWN")
 
 def indonesia(update, context):
+    realtime(update, 'indonesia')
+    
+    waktu = datetime.datetime.now()
+    tanggal = waktu.strftime('%D')
     api = requests.get('https://api.kawalcorona.com/indonesia/')
     api_json = api.json()
     api_content = api_json
@@ -168,8 +193,10 @@ def indonesia(update, context):
 "Positif = *{}*\n"
 "Sembuh = *{}*\n"
 "Meninggal = *{}*\n"
-"Dirawat = *{}*\n"
-.format(negara,positif,sembuh,meninggal,dirawat))
+"Dirawat = *{}*\n\n"
+"UPDATE = *{}*"
+.format(negara,positif,sembuh,meninggal,dirawat,tanggal))
          update.message.reply_text(kirim, parse_mode="Markdown")
-
+    update.message.reply_text("Mari kita terus bersama-sama menangani pandemi ini, bergotong royong, bersatu padu karena hanya dengan cara kebersamaan ini kita akan dapat mengatasinya. Kita tidak sendiri, kita bersama dengan negara-negara lain yang juga mengalami hal yang sama untuk bersama mengatasi pandemi ini. Dan tetaplah bersabar, optimis, tetap disiplin berada di rumah, jaga jarak dalam berhubungan/berinteraksi dengan orang lain, hindari kerumunan, rajin cuci tangan, pakailah masker saat keluar rumah. Ketika kedisiplinan kuat itu kita lakukan, insyaallah kita akan kembali pada situasi dan kondisi normal dan dapat bertemu dengan saudara, bertemu dengan teman, bertemu dengan kerabat dan tetangga dalam situasi yang normal. Tapi untuk saat ini marilah kita tetap berada di rumah saja.\n\n"
+                              "Pesan Sumber: https://kemlu.go.id/portal/id/read/1608/pidato/pesan-kepada-masyarakat-indonesia-terkait-covid-19-10-april-2020")
     update.message.reply_text("Ketik /help untuk kembali ke *MENU FITUR*.",parse_mode="MARKDOWN")
