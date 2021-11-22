14d6e456a2c039cb4dfaea358951a49da02ffd09
AlfinSeptianEkaSaputra <91175708+AlfinSeptianEkaSaputra@users.noreply.github.com>
2021-11-22 12:50:07 +0700

    Update fitur.py

--- fitur.py
+++ fitur.py
@@ -3,13 +3,11 @@ import requests
 import datetime
 import json
 import pandas as pd
-import calendar
 from telegram.ext import *
-from telegram import *
+
 # =====================================
 data = pd.read_csv('data.csv')
-data.tanggal = pd.to_datetime(data.tanggal, dayfirst=True)
-data.tanggal = data.tanggal.dt.date
+
 # =====================================
 
 def start_command(update, context):
@@ -26,6 +24,7 @@ def help_command(update, context):
                               "Info Covid\n"
                               "/indonesia => _Kasus covid-19 di Indonesia_ 🇮🇩\n"
                               "/berita => _Berita Seputar Covid19_ 📺\n"
+                              "/deteksiberita => _Mendeteksi berita seputar covid, apakah hoax atau tidak_ 📺\n"
                               "/cuaca => _Prediksi cuaca di Indonesia maupun di Dunia_ ☁️\n\n"
                               "Anda bisa juga konsultasi gejala covid, saya akan membantu anda"
                               "Atau anda hanya ingin mengobrol? Tidak apa-apa. Saya akan menemani anda :)",
@@ -131,112 +130,26 @@ def cuaca(update, context):
 
     update.message.reply_text("Ketik /help untuk kembali ke *MENU FITUR*.", parse_mode="MARKDOWN")
 
-print('Block 1, function to show calendar, get from github')
-def create_callback_data(action, year, month, day):
-    """ Create the callback data associated to each button"""
-    return ";".join([action, str(year), str(month), str(day)])
+def hasilcarijudulhoax(update, context):
+    pesan = update.message.text.strip().lower()
 
+    datum = data[data['judul'].str.lower().str.contains(pesan)].copy()
+    text = 'Berikut adalah artikel yang saya temukan' + '\n\n'
+    for row in datum.itertuples():
 
-def separate_callback_data(data):
-    """ Separate the callback data"""
-    return data.split(";")
+        text += '[' + str(row.tanggal) + ']' + '\n' + row.judul + '\n' + row.link + '\n\n'
+        if len(text) > 3500:
+            print(pesan, len(text))
+            update.message.reply_text(text)
+            text = ''
 
+    print(pesan, len(text))
+    update.message.reply_text(text)
 
-def create_calendar(year=None, month=None):
-    """
-    Create an inline keyboard with the provided year and month
-    :param int year: Year to use in the calendar, if None the current year is used.
-    :param int month: Month to use in the calendar, if None the current month is used.
-    :return: Returns the InlineKeyboardMarkup object with the calendar.
-    """
-    now = datetime.datetime.now()
-    if year == None: year = now.year
-    if month == None: month = now.month
-    data_ignore = create_callback_data("IGNORE", year, month, 0)
-    keyboard = []
-    # First row - Month and Year
-    row = []
-    row.append(InlineKeyboardButton(calendar.month_name[month] + " " + str(year), callback_data=data_ignore))
-    keyboard.append(row)
-    # Second row - Week Days
-    row = []
-    for day in ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]:
-        row.append(InlineKeyboardButton(day, callback_data=data_ignore))
-    keyboard.append(row)
+    update.message.reply_text(
+        "Jika tidak terdapat artikel kemungkinan berita yang anda cari bukanlah *HOAX*", parse_mode="MARKDOWN")
+    update.message.reply_text(
+        "Ketik /help untuk kembali ke *MENU FITUR*, atau Ketik /deteksiberita untuk kembali mencari berita yang menurut anda kurang meyakinkan.",
+        parse_mode="MARKDOWN")
 
-    my_calendar = calendar.monthcalendar(year, month)
-    for week in my_calendar:
-        row = []
-        for day in week:
-            if(day == 0):
-                row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
-            else:
-                row.append(InlineKeyboardButton(str(day), callback_data=create_callback_data("DAY", year, month, day)))
-        keyboard.append(row)
-    # Last row - Buttons
-    row = []
-    row.append(InlineKeyboardButton("<", callback_data=create_callback_data("PREV-MONTH", year, month, day)))
-    row.append(InlineKeyboardButton(" ", callback_data=data_ignore))
-    row.append(InlineKeyboardButton(">", callback_data=create_callback_data("NEXT-MONTH", year, month, day)))
-    keyboard.append(row)
-
-    return InlineKeyboardMarkup(keyboard)
-
-
-def process_calendar_selection(bot, update):
-    """
-    Process the callback_query. This method generates a new calendar if forward or
-    backward is pressed. This method should be called inside a CallbackQueryHandler.
-    :param telegram.Bot bot: The bot, as provided by the CallbackQueryHandler
-    :param telegram.Update update: The update, as provided by the CallbackQueryHandler
-    :return: Returns a tuple (Boolean,datetime.datetime), indicating if a date is selected
-                and returning the date if so.
-    """
-    ret_data = (False, None)
-    query = update.callback_query
-    (action, year, month, day) = separate_callback_data(query.data)
-    curr = datetime.datetime(int(year), int(month), 1)
-    if action == "IGNORE":
-        bot.answer_callback_query(callback_query_id=query.id)
-    elif action == "DAY":
-        bot.edit_message_text(text=query.message.text,
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id
-                              )
-        ret_data = True, datetime.datetime(int(year), int(month), int(day))
-    elif action == "PREV-MONTH":
-        pre = curr - datetime.timedelta(days=1)
-        bot.edit_message_text(text=query.message.text,
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=create_calendar(int(pre.year), int(pre.month)))
-    elif action == "NEXT-MONTH":
-        ne = curr + datetime.timedelta(days=31)
-        bot.edit_message_text(text=query.message.text,
-                              chat_id=query.message.chat_id,
-                              message_id=query.message.message_id,
-                              reply_markup=create_calendar(int(ne.year), int(ne.month)))
-    else:
-        bot.answer_callback_query(callback_query_id=query.id, text="Something went wrong!")
-        # UNKNOWN
-    return ret_data
-
-def caritanggalhoax(update, context):
-    update.message.reply_text("Please select a date: ",
-                              reply_markup=create_calendar())
-
-
-def hasilcaritanggalhoax(bot, update):
-    selected, date = process_calendar_selection(bot, update)
-    if selected:
-        datum = data[data.tanggal == date.date()]
-        if datum.empty:
-            text = 'Tidak terdapat artikel hoax yang terbit pada tanggal ' + date.strftime('%d-%m-%Y')
-        else:
-            text = 'Berikut adalah artikel hoax yang terbit pada tanggal ' + date.strftime('%d-%m-%Y') + '\n\n'
-            for row in datum.itertuples():
-                text += '[' + str(row.tanggal) + ']' + '\n' + row.judul + '\n' + row.link + '\n\n'
-        bot.send_message(chat_id=update.callback_query.from_user.id,
-                         text=text,
-                         reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
